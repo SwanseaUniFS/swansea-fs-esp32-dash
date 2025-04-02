c9b90abd5e34ee3367a0a620c210683c734044ba
Ethan Maya <92396506+EthanMaya@users.noreply.github.com>
2025-04-02 17:00:18 +0100

    still not working

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,24 @@
 //works   38 44 43
 //doesn't 19
 typedef u_int8_t u8;
-
+typedef u_int16_t u16;
 CanFrame rxFrame;
+
 void setup()
 {
-  Serial.begin(9600);
-  auto SUCCESS =
+
+
+  auto SUCCESS = 
       ESP32Can.begin(ESP32Can.convertSpeed(SPEED), CAN_TX, CAN_RX, 10, 10);
     if (SUCCESS) {
       Serial.println("CAN bus started!");
     } else {
       Serial.println("CAN bus failed!");
     }
-  #if (HAS_DISPLAY)
+    
+#if (HAS_DISPLAY)
     init_screen();
   #endif
-    //pinMode(P, OUTPUT);
-    //digitalWrite(P, LOW);
 }
 
 #if (HAS_DISPLAY)
@@ -39,24 +40,68 @@ lv_obj_t **e_Pressure = &ui_Label6;
 int y = 0;
 #endif
 
-u8 throttle = 0;
+u16 throttle = 0;
 u8 temperature = 0;
 u8 pressure = 0;
 u8 gear = -1;
 u8 temp = 0;
 void loop()
-{
-    if (ESP32Can.readFrame(rxFrame, 1000)) {
-      throttle = rxFrame.data[0];
-      temperature = rxFrame.data[1];
-      gear = rxFrame.data[2];
-      pressure = rxFrame.data[3];
+{ //while
+  //0x360 rpm 0-1
+  //0x360 throttle 4-5
+  //0x361 oil press 0-1
+  //0x3E0 6-7 oil temp
+  //0x470 6 gear selector position
+  auto SUCCESS = 
+      ESP32Can.begin(ESP32Can.convertSpeed(SPEED), CAN_TX, CAN_RX, 10, 10);
+  u8 temp = rxFrame.identifier;
+  ESP32Can.end();
+  Serial.begin(9600);
+  Serial.println(temp);
+  Serial.println("Serial");
+  Serial.end();
 
-      Serial.printf("Throttle %d \r\n", throttle);
-      Serial.printf("Temperature %d \r\n", temperature);
-      Serial.printf("Gear %d \r\n", gear);
-      Serial.printf("Pressure %d \r\n\n", pressure);
-    }
+  
+
+
+  // boolean nothingToOutput = true;
+  // while (nothingToOutput) {
+  //   Serial.println("No output");
+  //   if (ESP32Can.readFrame(rxFrame, 1000))
+  //   {
+  //     Serial.println(rxFrame.identifier);
+
+  //     if (rxFrame.identifier == 0x360) { //rpm
+  //       u16 bit0 = rxFrame.data[4];
+  //       u16 bit1 = rxFrame.data[5];
+  //       throttle = (bit0 << 8) | bit1;
+  //       throttle /= 10;
+  //       nothingToOutput = false;
+  //     }
+  //     if (rxFrame.identifier == 0x471) { //rpm
+  //       u16 bit0 = rxFrame.data[2];
+  //       u16 bit1 = rxFrame.data[3];
+  //       temperature = (bit0 << 8) | bit1;
+  //       temperature /= 10;
+  //       nothingToOutput = false;
+  //     }
+  //     if (rxFrame.identifier == 0x361) { //oil press
+  //       temperature = rxFrame.data[1];
+  //     }
+  //     if (rxFrame.identifier == 0x3E0) {//oil temp
+  //       gear = rxFrame.data[2];
+  //     }
+  //     if (rxFrame.identifier == 0x470) { //gear sel
+  //       pressure = rxFrame.data[3];
+  //     }
+  //     //Serial.printf("ID: %d", rxFrame.identifier);
+  //     Serial.printf("Throttle %d \r\n", throttle);
+  //     Serial.printf("Temperature %d \r\n", temperature);
+  //     Serial.printf("Gear %d \r\n", gear);
+  //     Serial.printf("Pressure %d \r\n\n", pressure);
+  //   }
+  //   nothingToOutput = false;
+  // }
 
 #if (HAS_DISPLAY)
     char buf[16];
@@ -71,19 +116,21 @@ void loop()
     lv_label_set_text(*e_Pressure, buf);
     
 
-    temp++;
+    // temp++;
 
-    Serial.println(temp);
+    // Serial1.println(temp);
 
-    Serial.println("before GUI");
+    // Serial1.println("before GUI");
 
     lv_timer_handler(); /* let the GUI do its work */
-    Serial.println("After GUI");
+    //Serial1.println("After GUI");
     //digitalWrite(P, HIGH);
     //delay(1000);
     //digitalWrite(P, LOW);
     delay(10);
 #endif
+
+
 }
 
 // #include "ui.h"

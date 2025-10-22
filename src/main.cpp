2c88f3211481d2ad3e425adc3679d6f610662123
Ethan Maya <92396506+EthanMaya@users.noreply.github.com>
2025-10-22 16:03:36 +0100

    added in led stuff into main (doesn't work

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "config.h"
 #include <cstdint>
 #include <cstdio>
+#include <Adafruit_NeoPixel.h>
+
 
 #define SPEED 1000
 #define HAS_DISPLAY 1
@@ -17,13 +19,30 @@
 #include <lvgl.h>
 #define CAN_TX 44
 #define CAN_RX 43
+#define LED_PIN  38          
 #else
 #define CAN_TX 5
 #define CAN_RX 4
+#define LED_PIN  6        
 #endif
 
 using u8 = uint8_t;
 using u16 = uint16_t;
+
+#define NUM_LEDS 20          
+#define FLICKER_INTERVAL 20 
+
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+double observed_rpm_min = 0;
+double observed_rpm_max = RPM_MAX * 1.2; 
+unsigned long last_flash_time = 0;
+bool flash_state = true;
+int last_leds_to_light = 0;
+
+
+double rpm;
+
 
 void handle_speed(const CanFrame &rxFrame);
 void handle_rpm(const CanFrame &rxFrame);
@@ -43,6 +62,52 @@ public:
     return rxFrame.identifier == identifier;
   }
 };
+
+void updateRPMLEDs(double rpm) {
+  unsigned long now = millis();
+  if (observed_rpm_max <= observed_rpm_min + 1) return;
+
+  int leds_to_light = map(
+      constrain(rpm, observed_rpm_min, observed_rpm_max),
+      observed_rpm_min, observed_rpm_max, 0, NUM_LEDS);
+
+  int green_zone_end  = NUM_LEDS * 0.25;
+  int yellow_zone_end = NUM_LEDS * 0.50;
+  int red_zone_end    = map(RPM_MAX, observed_rpm_min, observed_rpm_max, 0, NUM_LEDS);
+  if (red_zone_end < yellow_zone_end) red_zone_end = yellow_zone_end;
+  if (red_zone_end > NUM_LEDS) red_zone_end = NUM_LEDS;
+
+  last_leds_to_light = leds_to_light;
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (i < leds_to_light) {
+      if (i < green_zone_end)
+        strip.setPixelColor(i, strip.Color(0, 255, 0));          
+      else if (i < yellow_zone_end)
+        strip.setPixelColor(i, strip.Color(255, 255, 0));         
+      else if (i < red_zone_end)
+        strip.setPixelColor(i, strip.Color(255, 0, 0));          
+      else
+        strip.setPixelColor(i, strip.Color(180, 0, 255));         
+    } else {
+      strip.setPixelColor(i, 0, 0, 0);
+    }
+  }
+
+  if (rpm > RPM_MAX) {
+    if (now - last_flash_time > FLICKER_INTERVAL) {
+      flash_state = !flash_state;
+      last_flash_time = now;
+    }
+
+    if (!flash_state) {
+      for (int i = 0; i < NUM_LEDS; i++) strip.setPixelColor(i, 0, 0, 0);
+    }
+  }
+
+  strip.show();
+}
+
 
 char buf[32];
 CanFrame rxFrame;
@@ -154,6 +219,10 @@ void setup() {
     Serial.println("CAN bus failed!");
   }
 
+  strip.begin();
+  strip.show();
+  strip.setBrightness(20);
+
   rule_engine.add_rule(CompareIdentifier(0x370), &handle_speed);
   rule_engine.add_rule(CompareIdentifier(0x360), &handle_rpm);
   rule_engine.add_rule(CompareIdentifier(0x372), &handle_engine_voltage);
@@ -173,7 +242,7 @@ void loop() {
   update = false;
 
   if (ESP32Can.readFrame(rxFrame, 1000)) {
-    rule_engine.run(rxFrame);
+    rule_engine.run(rxFrame);  
   }
 
 #if (HAS_DISPLAY)
@@ -181,6 +250,7 @@ void loop() {
     lv_timer_handler();
   }
 #endif
+    updateRPMLEDs(rpm);
 }
 
 
@@ -200,13 +270,13 @@ void handle_speed(const CanFrame &rxFrame) {
 
 void handle_rpm(const CanFrame &rxFrame) {
   u16 raw = ((u16)rxFrame.data[0] << 8) | (u16)rxFrame.data[1];
-  double rpm_val = (double)raw;
+  rpm = (double)raw;
 
 #if (HAS_DISPLAY)
   update_text_u16((u16)raw, ui_erpm);
   lv_bar_set_value(ui_erpmbar, raw, LV_ANIM_OFF);
-  toggle_max_threshold(rpm_val, (double)RPM_MAX, rpm_up);
-  toggle_min_threshold(rpm_val, (double)RPM_MIN, rpm_down);
+  toggle_max_threshold(rpm, (double)RPM_MAX, rpm_up);
+  toggle_min_threshold(rpm, (double)RPM_MIN, rpm_down);
   display_update_rpm();
 #else
   Serial.printf("RPM: %.0f\n", rpm_val);
